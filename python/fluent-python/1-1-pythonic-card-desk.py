ef8cf52f70c3bd7fc3ae01eed382e99441cd5c88
vlad <vladbeklenyshchev@gmail.com>
2016-09-25 15:42:28 +0300

    example 1-1 finished

--- python/fluent-python/1-1-pythonic-card-desk.py
+++ python/fluent-python/1-1-pythonic-card-desk.py
@@ -16,4 +16,9 @@ class FrenchDeck:
 		return len(self._cards)
 	
 	def __getitem__(self, position):
-		return self._cards[position] 
+		return self._cards[position]
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+def spades_high(card):
+	rank_value = FrenchDeck.ranks.index(card.rank)
+	return rank_value * len(suit_values) + suit_values[card.suit]
