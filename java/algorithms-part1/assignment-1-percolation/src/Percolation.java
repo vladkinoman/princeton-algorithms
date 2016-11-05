bf4abe717adfcbc994861190b9f38343bc724ba6
vlad <vladbeklenyshchev@gmail.com>
2016-11-05 18:49:32 +0200

    Started task Percolation

---
+++ java/algorithms-part1/assignment-1-percolation/src/Percolation.java
@@ -0,0 +1,35 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    // create n-by-n grid, with all sites blocked
+    public Percolation(int n){
+
+    }
+
+    // open site (row, col) if it is not open already
+    public void open(int row, int col){
+
+    }
+
+    // is site (row, col) open?
+    public boolean isOpen(int row, int col){
+        return true;
+    }
+
+    // is site (row, col) full?
+    public boolean isFull(int row, int col){
+        return true;
+    }
+
+    // does the system percolate?
+    public boolean percolates(){
+        return true;
+    }
+
+    // test client (optional)
+    public static void main(String[] args){
+
+    }
+}
---
+++ java/algorithms-part1/assignment-1-percolation/src/PercolationStats.java
@@ -0,0 +1,35 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class PercolationStats {
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean(){
+        return 0.0;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev(){
+        return 0.0;
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo(){
+        return 0.0;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi(){
+        return 0.0;
+    }
+
+    // test client (described below)
+    public static void main(String[] args) {
+
+    }
+}
