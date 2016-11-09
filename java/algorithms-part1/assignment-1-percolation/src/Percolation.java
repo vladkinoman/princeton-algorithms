2182dbfe0764d6ed62b08ecce734ee366dcf2bc6
vlad <vladbeklenyshchev@gmail.com>
2016-11-09 13:55:36 +0200

    Final refactor

--- java/algorithms-part1/assignment-1-percolation/src/Percolation.java
+++ java/algorithms-part1/assignment-1-percolation/src/Percolation.java
@@ -12,8 +12,6 @@
  *
  *----------------------------------------------------------------*/
 import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
--- java/algorithms-part1/assignment-1-percolation/src/PercolationStats.java
+++ java/algorithms-part1/assignment-1-percolation/src/PercolationStats.java
@@ -1,54 +1,53 @@
 import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class PercolationStats {
-    private int n;
-    private int trials;
+    private int experimentsCount;
+    private int trialsCount;
     private double xMean;
     private double stddevValue;
     private double confLowValue;
     private double confHighValue;
 
     // perform trials independent experiments on an n-by-n grid
-    public PercolationStats(int n, int trials) {
-        if (n <= 0 || trials <= 0) {
+    public PercolationStats(int n, int t) {
+        if (n <= 0 || t <= 0) {
             throw new java.lang.IllegalArgumentException("Input correct args.");
         }
-        this.n = n;
-        this.trials = trials;
+        experimentsCount = n;
+        trialsCount = t;
 
         int posX = 0;
         int posY = 0;
-        double[] openSites = new double[trials];
+        double[] openSites = new double[trialsCount];
         int osCount = 0;
-        for (int i = 0; i < trials; i++, osCount = 0) {
-            Percolation pObj = new Percolation(n);
+        for (int i = 0; i < trialsCount; i++, osCount = 0) {
+            Percolation pObj = new Percolation(experimentsCount);
             while (!pObj.percolates()) {
-                posX = StdRandom.uniform(1, n + 1);
-                posY = StdRandom.uniform(1, n + 1);
+                posX = StdRandom.uniform(1, experimentsCount + 1);
+                posY = StdRandom.uniform(1, experimentsCount + 1);
                 if (!pObj.isOpen(posX, posY)) {
                     pObj.open(posX, posY);
                     osCount++;
                 }
             }
-            openSites[i] = (double) (osCount ) / (n * n);
+            openSites[i] = (double) (osCount) / (experimentsCount
+                    * experimentsCount);
             xMean += openSites[i];
         }
 
-        xMean /= (double)trials;
-        for (int i = 0; i < trials; i++) {
+        xMean /= (double) trialsCount;
+        for (int i = 0; i < trialsCount; i++) {
             stddevValue += (openSites[i] - xMean) * (openSites[i] - xMean);
         }
-        stddevValue /= (double)trials - 1.0;
+        stddevValue /= (double) trialsCount - 1.0;
         confLowValue = xMean - ((1.96 * Math.sqrt(stddevValue))
-                / Math.sqrt(trials));
+                / Math.sqrt(trialsCount));
         confHighValue = xMean + ((1.96 * Math.sqrt(stddevValue))
-                / Math.sqrt(trials));
+                / Math.sqrt(trialsCount));
     }
 
     // sample mean of percolation threshold
-    public double mean(){
+    public double mean() {
         return xMean;
     }
 
@@ -58,20 +57,20 @@ public class PercolationStats {
     }
 
     // low  endpoint of 95% confidence interval
-    public double confidenceLo(){
+    public double confidenceLo() {
         return confLowValue;
     }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi(){
+    public double confidenceHi() {
         return confHighValue;
     }
 
     // test client (described below)
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int T = Integer.parseInt(args[1]);
-        PercolationStats psObj = new PercolationStats(n, T);
+        int t = Integer.parseInt(args[1]);
+        PercolationStats psObj = new PercolationStats(n, t);
         System.out.println("mean = " + psObj.mean());
         System.out.println("stddev = " + psObj.stddev());
         System.out.print("95% confidence interval = " + psObj.confidenceLo() +
