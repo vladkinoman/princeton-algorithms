f3810467556bfb35c1aa348ace350e2e1d9e5053
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2016-12-04 22:09:37 +0200

    Added proof (first version)

--- proving-programs-correct/lab_4_var_1.c
+++ proving-programs-correct/lab_4_var_1.c
@@ -1,21 +1,62 @@
-int count(int * a, int N)
-{
-	int x = 0; 
-	int i = 0;
+#include <stdio.h>
+#include <string.h>
 
-	while (i < N)
+
+/*@
+    axiomatic CountAxiomatic
+    {
+        logic int Count{L} (int a[], integer N)
+            reads a[0..N-1];
+        
+        axiom CountEmpty:
+            \forall int a[], integer N;
+                N <= 0 ==> Count(a, N) == 0;
+
+        axiom CountOneHit:
+            \forall int a[], integer N;
+                N % 2 == 0 && a[N] % 2 != 0 ==> Count(a, N + 1) == Count(a, N) + 1;
+        
+        axiom CountOneMiss:
+            \forall int a[], integer N;
+                !(N % 2 == 0 && a[N] % 2 != 0) ==> Count(a, N + 1) == Count(a, N);
+    }
+*/
+
+/*@ requires N >= 0 && \valid_read(a + (0..N-1));
+    
+    assigns \nothing;
+    
+    ensures \result == Count(a, N);
+    ensures 0 <= \result < N;
+*/
+int count(int a[], int N)
+{
+	int i, x;
+	/*@ loop invariant 0 <= i <= N;
+	    loop invariant 0 <= x <= i;
+	    loop invariant x == Count(a, i);
+	    loop assigns i, x;
+	    loop variant N - i;
+	*/
+	for (i = 0, x = 0; i < N; i++)
 	{
-		if (i % 2 != 0 && a[i] % 2 != 0)
+		if (i % 2 == 0 && a[i] % 2 != 0)
 		{
 			x = x + 1;
-			i = i + 1;
-		}
-		else
-		{
-			i = i + 1;
 		}
 	}
 
 	return x;
 }
+
+int main()
+{
+    	// count of odd entries that are at the even positions
+    	int N = 6;
+    	int a[N];
+    	memcpy(a, (int[]) {23, 50, 61, 124, 562, 1000}, sizeof a);
+	int result = count(a, N);
+	printf("result = %i\n", result);
 	
+	return 0;
+}
