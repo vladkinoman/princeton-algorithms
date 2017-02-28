3e8a1ed4e2e18fb1ccae2bf195000d85af672673
vlad <vladbeklenyshchev@gmail.com>
2017-02-28 22:36:56 +0200

    Rename directory for coursera algorithms: part 1

--- ocaml/reverse.ml
+++
@@ -1,6 +0,0 @@
-let rev list =
-    let rec aux acc = function
-      | [] -> acc
-      | h::t -> aux (h::acc) t in
-    aux [] list
-;;
--- ocaml/task1_sum.ml
+++
@@ -1,8 +0,0 @@
-open Printf
-
-let sum a b =
-(a + b);;
-
-let () = 
-		printf "%i\n" (sum (int_of_string Sys.argv.(1)) (int_of_string Sys.argv.(2)))
-		;;
