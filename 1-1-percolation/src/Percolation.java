03b3fd27d2495b5e263bfa5e9c6a1399cfadcb1f
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-08-08 19:34:52 +0300

    Add folder for the 1st assignment, everything is ready for coding

---
+++ 1-1-percolation/src/Percolation.java
@@ -0,0 +1,41 @@
+public class Percolation {
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n){
+
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col){
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col){
+
+        return false;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col){
+
+        return false;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites(){
+
+        return 0;
+    }
+
+    // does the system percolate?
+    public boolean percolates(){
+
+        return false;
+    }
+
+    // test client (optional)
+    public static void main(String[] args){
+        System.out.println("Hello, World!!!");
+    }
+}
---
+++ 1-1-percolation/src/PercolationStats.java
@@ -0,0 +1,37 @@
+public class PercolationStats {
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials){
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean(){
+
+        return 0;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev(){
+
+        return 0;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo(){
+
+        return 0;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi(){
+
+        return 0;
+    }
+
+    // test client (see below)
+    public static void main(String[] args){
+
+    }
+
+}
