1a432c088f3ad045b95203ac8d710e1a0536ef3e
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-08-11 17:46:11 +0300

    Add checkstyle plugin configuration and add style to the Percolation.java file

--- 1-1-percolation/src/Percolation.java
+++ 1-1-percolation/src/Percolation.java
@@ -1,54 +1,72 @@
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ * The {@code PercolationStats} class provides methods
+ * for creating percolation system with two WeightedQuickUnion Data Structures
+ * which track opened blocks, add them to components,
+ * check whether block is full or system percolates.
+ *
+ * @author Vlad Beklenyshchev aka vladkinoman
+ */
 public class Percolation {
-    boolean grid[][];
-    int n;
-    int openSites;
-    WeightedQuickUnionUF ufDataStructure;
-    int virtualTopSite;
-    int virtualBottomSite;
-    WeightedQuickUnionUF ufDataStructureWithTwoExtraSites;
+    private boolean grid[][];
+    private int n;
+    private int openSites;
+    private WeightedQuickUnionUF ufDataStructure;
+    private int virtualTopSite;
+    private int virtualBottomSite;
+    private WeightedQuickUnionUF ufDataStructureWithTwoExtraSites;
 
-    // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n){
+    /**
+     * Creates n-by-n grid, with all sites initially blocked.
+     * Performance: time proportional to  4n =  2n + 2n.
+     *
+     * @param n width of the grid
+     */
+    public Percolation(int n) {
         if (n <= 0)
             throw new IllegalArgumentException();
-
+        this.n = n;
+        grid = new boolean[n][n];
         virtualTopSite = n * n;
         virtualBottomSite = n * n + 1;
+
         // using wqu version with 2 additional virtual sites
-        ufDataStructure = new WeightedQuickUnionUF( n * n);
-        ufDataStructureWithTwoExtraSites = new WeightedQuickUnionUF(n * n + 2);
-        grid = new boolean[n][];
-        this.n = n;
-        for (int i = 0; i < n; i++) {
-                grid[i] = new boolean[n];
-                for (int j = 0; j < n; j++)
-                    grid[i][j] = false;
-        }
+        ufDataStructure = new WeightedQuickUnionUF(n * n); // n
+        ufDataStructureWithTwoExtraSites =
+                new WeightedQuickUnionUF(n * n + 2);       // n
 
         // connect top row to the virtual top site
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)                           // n
             ufDataStructureWithTwoExtraSites.union(i, virtualTopSite);
 
         // connect bottom row to the virtual bottom site
-        for (int i = 0; i < n; i++)
-            ufDataStructureWithTwoExtraSites.union(n * (n - 1) + i, virtualBottomSite);
-
+        for (int i = 0; i < n; i++)                          // n
+            ufDataStructureWithTwoExtraSites.union(n * (n - 1) + i,
+                    virtualBottomSite);
     }
 
-    // opens the site (row, col) if it is not open already
-    public void open(int row, int col){
-        // elements are counted from 1 to n.
+    /**
+     * Opens the site (row, col) if it is not open already.
+     * Performance: constant time plus a constant number of calls to the uf method union()
+     * We have 2 * 4 = 8 calls to the union() method in the worst case.
+     * Notice that row and col are counted from 1 to n.
+     *
+     * @param row row number of the element
+     * @param col column number of the element
+     */
+    public void open(int row, int col) {
+
         int shiftedRow = row - 1;
         int shiftedCol = col - 1;
 
-        if(shiftedRow < 0 || shiftedRow >= n
-          || shiftedCol < 0 || shiftedCol >= n)
+        if (shiftedRow < 0 || shiftedRow >= n
+                || shiftedCol < 0 || shiftedCol >= n)
             throw new IllegalArgumentException();
 
-        if(!grid[shiftedRow][shiftedCol])
-        {
+        if (!grid[shiftedRow][shiftedCol]) {
+
             grid[shiftedRow][shiftedCol] = true;
 
             int curr = shiftedRow * n + shiftedCol;
@@ -57,62 +75,98 @@ public class Percolation {
             int left = shiftedRow * n + shiftedCol - 1;
             int bottom = (shiftedRow + 1) * n + shiftedCol;
 
-            if(shiftedCol + 1 < n && grid[shiftedRow][shiftedCol + 1]) {
+            if (shiftedCol + 1 < n && grid[shiftedRow][shiftedCol + 1]) {
                 ufDataStructure.union(curr, right);
                 ufDataStructureWithTwoExtraSites.union(curr, right);
             }
 
-            if(shiftedRow - 1 >= 0 && grid[shiftedRow - 1][shiftedCol]) {
+            if (shiftedRow - 1 >= 0 && grid[shiftedRow - 1][shiftedCol]) {
                 ufDataStructure.union(curr, up);
                 ufDataStructureWithTwoExtraSites.union(curr, up);
             }
 
-            if(shiftedCol - 1 >= 0 && grid[shiftedRow][shiftedCol - 1]) {
+            if (shiftedCol - 1 >= 0 && grid[shiftedRow][shiftedCol - 1]) {
                 ufDataStructure.union(curr, left);
                 ufDataStructureWithTwoExtraSites.union(curr, left);
             }
 
-            if(shiftedRow + 1 < n && grid[shiftedRow + 1][shiftedCol]) {
+            if (shiftedRow + 1 < n && grid[shiftedRow + 1][shiftedCol]) {
                 ufDataStructure.union(curr, bottom);
                 ufDataStructureWithTwoExtraSites.union(curr, bottom);
             }
 
             openSites++;
         }
-
     }
 
-    // is the site (row, col) open?
-    public boolean isOpen(int row, int col){
+    /**
+     * Returns a boolean value which is true when the site (row, col) is open
+     * and returns false otherwise.
+     * Performance: constant time.
+     *
+     * @param row row number of the element
+     * @param col column number of the element
+     * @return {@code true} if the site is full, {@code false} otherwise
+     */
+    public boolean isOpen(int row, int col) {
         int shiftedRow = row - 1;
         int shiftedCol = col - 1;
 
-        if(shiftedRow < 0 || shiftedRow >= n
+        if (shiftedRow < 0 || shiftedRow >= n
                 || shiftedCol < 0 || shiftedCol >= n)
             throw new IllegalArgumentException();
 
         return grid[shiftedRow][shiftedCol];
     }
 
-    // is the site (row, col) full?
-    public boolean isFull(int row, int col){
+    /**
+     * Returns a boolean value which is true when the site (row, col) is full
+     * (flooded with water / painted blue) and returns false otherwise.
+     * Performance: n calls to the uf method connected() in the worst case :/
+     *
+     * @param row row number of the element
+     * @param col column number of the element
+     * @return {@code true} if the site is full, {@code false} otherwise
+     */
+    public boolean isFull(int row, int col) {
         if (isOpen(row, col))
             for (int i = 0; i < n; i++)
-                if(ufDataStructure.connected((row - 1) * n + (col - 1), i))
+                // worst case scenario: when all blocks of the top row are opened
+                // and possible connected block is the last one
+                if (grid[0][i] && ufDataStructure.connected((row - 1) * n
+                        + (col - 1), i))
                     return true;
         return false;
     }
 
-    // returns the number of open sites
-    public int numberOfOpenSites(){ return openSites; }
-
-    // does the system percolate?
-    public boolean percolates(){
-        return n != 1 ? ufDataStructureWithTwoExtraSites.connected(virtualTopSite, virtualBottomSite) : grid[0][0];
+    /**
+     * Returns the number of open sites.
+     * Performance: constant time.
+     *
+     * @return an integer value of the number of open sites.
+     */
+    public int numberOfOpenSites() {
+        return openSites;
     }
 
-    // test client (optional)
-    public static void main(String[] args){
+    /**
+     * Returns a boolean value which is true when the system percolates
+     * and false otherwise.
+     * Performance: only 1 call to the uf method connected().
+     *
+     * @return {@code true} if the system percolates, {@code false} otherwise
+     */
+    public boolean percolates() {
+        return n != 1 ? ufDataStructureWithTwoExtraSites.connected(
+                virtualTopSite, virtualBottomSite) : grid[0][0];
+    }
+
+    /**
+     * Test client for Percolation.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
         Percolation newSystem = new Percolation(1);
         System.out.println(newSystem.percolates());
     }
