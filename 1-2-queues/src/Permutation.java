11a5b66b104497bbe53d66127bec4c4a61225984
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-08-28 11:21:36 +0300

    Add Permutation.java which test either Deque or RandomizedQueue classes

--- 1-2-queues/src/Permutation.java
+++ 1-2-queues/src/Permutation.java
@@ -14,11 +14,24 @@ import edu.princeton.cs.algs4.StdOut;
 public class Permutation {
 
     /**
-     * Unit tests the {@code Deque} and {@code RandomizedQueue} data types.
+     * Unit tests the {@code Deque} or {@code RandomizedQueue} data types.
      *
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
+
+        RandomizedQueue<String> randqueue = new RandomizedQueue<>();
+        while (!StdIn.isEmpty())
+            randqueue.enqueue(StdIn.readString());
+
+        int k = Integer.parseInt(args[0]);
+        if (k < 0 || k > randqueue.size())
+            throw new IllegalArgumentException();
+
+        for (String s : randqueue) {
+            StdOut.println(s);
+            if (--k == 0) break;
+        }
 
     }
 }
