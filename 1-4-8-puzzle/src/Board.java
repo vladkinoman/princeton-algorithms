4c7626d32c06e890d3c44afe4ba9fbffc0f32f08
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-10-10 23:03:57 +0300

    Add files for assignment-1-4

---
+++ 1-4-8-puzzle/src/Board.java
@@ -0,0 +1,54 @@
+public class Board {
+
+    // create a board from an n-by-n array of tiles,
+    // where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles) {
+
+    }
+
+    // string representation of this board
+    public String toString() {
+        return null;
+    }
+
+    // board dimension n
+    public int dimension() {
+        return 0;
+    }
+
+    // number of tiles out of place
+    public int hamming() {
+        return 0;
+    }
+
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan() {
+        return 0;
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        return false;
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+        return null;
+    }
+
+    // a board that is obtained by exchanging any pair of tiles
+    public Board twin() {
+        return null;
+    }
+
+    // unit testing (not graded)
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ 1-4-8-puzzle/src/Solver.java
@@ -0,0 +1,49 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+public class Solver {
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+        return false;
+    }
+
+    // min number of moves to solve initial board
+    public int moves() {
+        return 0;
+    }
+
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution() {
+        return null;
+    }
+
+    // test client
+    public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }
+
+}
