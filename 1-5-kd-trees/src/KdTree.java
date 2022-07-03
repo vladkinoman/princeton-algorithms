dc7789b1e90802f56325fdb6b55bfe349e817134
vladkinoman <vladbeklenyshchev@gmail.com>
2022-07-03 19:58:01 +0300

    add files for the assignment 1-5 and change .gitignore to untrack .class files

---
+++ 1-5-kd-trees/src/KdTree.java
@@ -0,0 +1,19 @@
+import edu.princeton.cs.algs4.*;
+
+public class KdTree {
+
+    public void insert(Point2D p) {
+    }
+
+    public void draw() {
+    }
+
+    public Point2D nearest(Point2D query) {
+        return null;
+    }
+
+    public Point2D[] range(RectHV rect) {
+        return null;
+    }
+    
+}
---
+++ 1-5-kd-trees/src/PointSET.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET {
+
+    public void insert(Point2D p) {
+    }
+
+    public void draw() {
+    }
+
+    public Point2D nearest(Point2D query) {
+        return null;
+    }
+
+    public Point2D[] range(RectHV rect) {
+        return null;
+    }
+    
+}
