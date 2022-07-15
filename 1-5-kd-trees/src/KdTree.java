8dc6b223a68c943f7dfb9fa6c10f9263f1b17cf6
vladkinoman <vladbeklenyshchev@gmail.com>
2022-07-15 15:45:31 +0300

    add implementation of the draw method

--- 1-5-kd-trees/src/KdTree.java
+++ 1-5-kd-trees/src/KdTree.java
@@ -5,11 +5,16 @@ import java.util.List;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
 
     private static class Node {
+    
+        public Node(Point2D newp) {
+            p = newp;
+        }
         private Point2D p;
         private Node lb;
         private Node rt;
@@ -20,7 +25,7 @@ public class KdTree {
     /** Construct an empty set of points.
      */
     public KdTree() {
-        root = new Node();
+        root = null;
     }
 
     /** Is the set empty?
@@ -47,22 +52,18 @@ public class KdTree {
      */
     public void insert(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
-        insert(root, p, true);
+        root = insert(root, p, true);
     }
 
     private Node insert(Node curr, Point2D newp, boolean doWeUseXAsKey) {
-        if (curr == null) {
-            curr = new Node();
-            curr.p = newp;
-            return curr;
-        }
+        if (curr == null) return new Node(newp);
         
         int cmp = 0;
         if (doWeUseXAsKey) {
-            // | y is fixed
+            // | x is fixed
             cmp = Double.compare(newp.x(), curr.p.x());
         } else {
-            // — x is fixed
+            // — y is fixed
             cmp = Double.compare(newp.y(), curr.p.y());
         }
 
@@ -85,10 +86,10 @@ public class KdTree {
         while (x != null) {
             int cmp = 0;
             if (doWeUseXAsKey) {
-                // | y is fixed
+                // | x is fixed
                 cmp = Double.compare(p.x(), x.p.x());
             } else {
-                // — x is fixed
+                // — y is fixed
                 cmp = Double.compare(p.y(), x.p.y());
             }
             if (cmp  < 0) x = x.lb;
@@ -103,7 +104,44 @@ public class KdTree {
      * 
      */
     public void draw() {
-        // todo
+        draw(root, new Point2D(0.0, 0.0), 
+                   new Point2D(1.0, 1.0), true);
+    }
+
+    /**
+     * 
+     * @param curr
+     * @param blp bottom left point of the partition
+     * @param trp top right point of the partition
+     * @param doWeUseXAsKey
+     */
+    private void draw(Node curr, Point2D blp, Point2D trp, boolean doWeUseXAsKey) {
+        if (curr == null) return;
+        
+        double x = curr.p.x();
+        double y = curr.p.y();
+
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.0205);
+        StdDraw.point(x, y);
+        StdDraw.setPenRadius();
+        if (doWeUseXAsKey) {
+            // | x is fixed
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.line(x, blp.y(), x, trp.y());
+            draw(curr.lb, new Point2D(blp.x(), blp.y()),
+                          new Point2D(      x, trp.y()), !doWeUseXAsKey);
+            draw(curr.rt, new Point2D(      x, blp.y()),
+                          new Point2D(trp.x(), trp.y()), !doWeUseXAsKey);
+        } else {
+            // — y is fixed
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.line(blp.x(), y, trp.x(), y);
+            draw(curr.lb, new Point2D(blp.x(), blp.y()),
+                          new Point2D(trp.x(),       y), !doWeUseXAsKey);
+            draw(curr.rt, new Point2D(blp.x(),       y),
+                          new Point2D(trp.x(), trp.y()), !doWeUseXAsKey);
+        }
     }
 
     private class PointsInRange implements Iterable<Point2D> {
@@ -112,9 +150,10 @@ public class KdTree {
 
         public PointsInRange(RectHV rect) {
             // inner constructor
-            for (Point2D p : rbBST) {
+            /*for (Point2D p : rbBST) {
                 if (rect.contains(p)) lPointsInRange.add(p);    
-            }
+            }*/
+            throw new UnsupportedOperationException();
         }
 
         @Override
@@ -140,7 +179,7 @@ public class KdTree {
      * @return
      */
     public Point2D nearest(Point2D to) {
-        if (to == null) throw new IllegalArgumentException();
+        /*if (to == null) throw new IllegalArgumentException();
         if (size() == 0) return null;
         
         Point2D pMinDist = rbBST.iterator().next();
@@ -150,6 +189,8 @@ public class KdTree {
             }
         }
         return pMinDist;
+        */
+        throw new UnsupportedOperationException();
     }
 
     /** Unit testing of the methods (optional)
@@ -157,9 +198,17 @@ public class KdTree {
      */
     public static void main(String[] args) {
         KdTree ps = new KdTree();
-        ps.insert(new Point2D(0.1, 0.1));
-        ps.insert(new Point2D(0.2, 0.5));
-        ps.insert(new Point2D(0.8, 0.9));
-        System.out.print(ps.nearest(new Point2D(0.5, 0.8)).toString());
+        ps.insert(new Point2D(0.7, 0.2));
+        ps.insert(new Point2D(0.5, 0.4));
+        ps.insert(new Point2D(0.2, 0.3));
+        ps.insert(new Point2D(0.4, 0.7));
+        ps.insert(new Point2D(0.9, 0.6));
+        // draw the points                                                        
+        StdDraw.enableDoubleBuffering();                                          
+        StdDraw.setXscale(0.0, 1.0);                                              
+        StdDraw.setYscale(0.0, 1.0); 
+        ps.draw();
+        StdDraw.show();
+        //System.out.print(ps.nearest(new Point2D(0.5, 0.8)).toString());
     }
 }
