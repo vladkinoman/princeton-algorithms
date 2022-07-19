c929285773cc54c592ad0ef308a27a4ad83356fe
vladkinoman <vladbeklenyshchev@gmail.com>
2022-07-19 19:10:12 +0300

    98/100! add count to Node and fix nearest

--- 1-5-kd-trees/src/KdTree.java
+++ 1-5-kd-trees/src/KdTree.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stopwatch;
+import edu.princeton.cs.algs4.Queue;
 
 /**
  * The {@code KdTree} represents a data type to represent a set of points in
@@ -33,10 +33,11 @@ public class KdTree {
         private Node lb;
         private Node rt;
         private final RectHV rect;
-
-        public Node(Point2D p, RectHV rect) {
+        private int count;
+        public Node(Point2D p, RectHV rect, int count) {
             this.p = p;
             this.rect = rect;
+            this.count = count;
         }
     }
 
@@ -46,7 +47,6 @@ public class KdTree {
      * Construct an empty set of points.
      */
     public KdTree() {
-        root = null;
     }
 
     /**
@@ -56,7 +56,7 @@ public class KdTree {
      *         {@code false} otherwise
      */
     public boolean isEmpty() {
-        return size() == 0;
+        return root == null;
     }
 
     /**
@@ -69,9 +69,8 @@ public class KdTree {
     }
 
     private int size(Node curr) {
-        if (curr == null)
-            return 0;
-        return 1 + size(curr.lb) + size(curr.rt);
+        if (curr == null) return 0;
+        return curr.count;
     }
 
     /**
@@ -91,8 +90,6 @@ public class KdTree {
         // we don't need a recursive version here,
         // b/c we don't need to update links on the way up
         while (x != null) {
-            if (x.p.equals(p))
-                return true;
             int cmp = 0;
             if (doWeUseXAsKey) {
                 // | x is fixed
@@ -101,9 +98,11 @@ public class KdTree {
                 // — y is fixed
                 cmp = Double.compare(p.y(), x.p.y());
             }
-            if (cmp < 0)
+            if      (cmp < 0)
                 x = x.lb;
-            else
+            else if (cmp == 0 && x.p.equals(p))
+                return true;
+            else 
                 x = x.rt;
 
             doWeUseXAsKey = !doWeUseXAsKey;
@@ -124,18 +123,16 @@ public class KdTree {
         root = insert(root, p, new RectHV(0.0, 0.0, 1.0, 1.0), true);
     }
 
-    private Node insert(Node curr, Point2D newp, RectHV rect,
+    private Node insert(Node curr, Point2D p, RectHV rect,
             boolean doWeUseXAsKey) {
         if (curr == null)
-            return new Node(newp, rect);
-        if (curr.p.equals(newp))
-            return curr;
-
+            return new Node(p, rect, 1);
+            
         int cmp = 0;
         RectHV newRect = null;
         if (doWeUseXAsKey) {
             // | x is fixed
-            cmp = Double.compare(newp.x(), curr.p.x());
+            cmp = Double.compare(p.x(), curr.p.x());
             if (cmp < 0) {
                 newRect = new RectHV(rect.xmin(), rect.ymin(), curr.p.x(), rect.ymax());
             } else {
@@ -143,7 +140,7 @@ public class KdTree {
             }
         } else {
             // — y is fixed
-            cmp = Double.compare(newp.y(), curr.p.y());
+            cmp = Double.compare(p.y(), curr.p.y());
             if (cmp < 0) {
                 newRect = new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), curr.p.y());
             } else {
@@ -151,11 +148,14 @@ public class KdTree {
             }
         }
 
-        if (cmp < 0)
-            curr.lb = insert(curr.lb, newp, newRect, !doWeUseXAsKey);
+        if      (cmp < 0)
+            curr.lb = insert(curr.lb, p, newRect, !doWeUseXAsKey);
+        else if (cmp == 0 && curr.p.equals(p))
+            curr.p = p;
         else
-            curr.rt = insert(curr.rt, newp, newRect, !doWeUseXAsKey);
+            curr.rt = insert(curr.rt, p, newRect, !doWeUseXAsKey);
 
+        curr.count = 1 + size(curr.lb) + size(curr.rt);
         return curr;
     }
 
@@ -197,15 +197,6 @@ public class KdTree {
         draw(curr.rt, !doWeUseXAsKey);
     }
 
-    private void buildList(Node curr, RectHV that, List<Point2D> lPointsInRange) {
-        if (curr == null || !curr.rect.intersects(that))
-            return;
-        buildList(curr.lb, that, lPointsInRange);
-        if (that.contains(curr.p))
-            lPointsInRange.add(curr.p);
-        buildList(curr.rt, that, lPointsInRange);
-    }
-
     /**
      * All points that are inside the rectangle (or on the boundary)
      * <p>
@@ -216,11 +207,35 @@ public class KdTree {
      * @return the points from the given range
      */
     public Iterable<Point2D> range(RectHV rect) {
+        // inorder traversal
         if (rect == null)
             throw new IllegalArgumentException();
-        List<Point2D> lPointsInRange = new ArrayList<>();
-        buildList(root, rect, lPointsInRange);
-        return lPointsInRange;
+        Queue<Point2D> qPointsInRange = new Queue<>();
+        buildQueue(root, rect, qPointsInRange);
+        return qPointsInRange;
+    }
+
+    private void buildQueue(Node curr, RectHV that, Queue<Point2D> q) {
+        if (curr == null || !curr.rect.intersects(that))
+            return;
+        buildQueue(curr.lb, that, q);
+        if (that.contains(curr.p))
+            q.enqueue(curr.p);
+        buildQueue(curr.rt, that, q);
+    }
+
+    /**
+     * A nearest neighbor in the set to point p; null if the set is empty
+     * <p>
+     * Running time: log N in the best case
+     * 
+     * @param p the point p which we are looking for the closest one
+     * @return the point which is the nearest to p
+     */
+    public Point2D nearest(Point2D p) {
+        if (p == null)
+            throw new IllegalArgumentException();
+        return root != null ? nearest(root, p, root.p, true) : null;
     }
 
     /**
@@ -233,44 +248,39 @@ public class KdTree {
      * the best one found so far.
      * 
      * @param curr    current node that is being checked
-     * @param queryp  the point to which we are looking for the closest point
-     * @param closest the current best point which is close to queryp
-     * @return
+     * @param p  the point p which we are looking for the closest point
+     * @param closest the current best point which is close to p
+     * @return the point which is the nearest to p
      */
-    private Point2D nearest(Node curr, Point2D queryp, Point2D closest) {
-        double dQuerypToClosest = queryp.distanceSquaredTo(closest);
-        // the pruning rule:
-        if (curr == null || curr.rect.distanceSquaredTo(queryp) > dQuerypToClosest)
-            return closest;
+    private Point2D nearest(Node curr, Point2D p, Point2D closest, boolean doWeUseXAsKey) {
+        double dClosest = p.distanceSquaredTo(closest);
+        double dCurr    = p.distanceSquaredTo(curr.p);
+        if (dCurr < dClosest) closest = curr.p;
 
-        if (curr.p.distanceSquaredTo(queryp) < dQuerypToClosest)
-            closest = curr.p;
-
-        Point2D closestInLB = nearest(curr.lb, queryp, closest);
-        double dQuerypToClosestInLB = queryp.distanceSquaredTo(closestInLB);
-        Point2D closestInRT = nearest(curr.rt, queryp, closest);
-        double dQuerypToClosestInRT = queryp.distanceSquaredTo(closestInRT);
-
-        if      (dQuerypToClosestInLB <= dQuerypToClosestInRT 
-            && dQuerypToClosestInLB < dQuerypToClosest) closest = closestInLB;
-        else if (dQuerypToClosestInRT <= dQuerypToClosestInLB 
-            && dQuerypToClosestInRT < dQuerypToClosest) closest = closestInRT;
+        if (doWeUseXAsKey && p.x() < curr.p.x()
+        || !doWeUseXAsKey && p.y() < curr.p.y()) {
+            // go to the left/down of the splitting line
+            if (curr.lb != null) {
+                closest  = nearest(curr.lb, p, closest, !doWeUseXAsKey);
+                dClosest = p.distanceSquaredTo(closest);
+            }
+            // should we check to the right/up of the splitting line?
+            if (curr.rt != null && curr.rt.rect.distanceSquaredTo(p) < dClosest) {
+                closest  = nearest(curr.rt, p, closest, !doWeUseXAsKey);
+            }
+        } else {
+            // go to the right/up of the splitting line
+            if (curr.rt != null) {
+                closest = nearest(curr.rt, p, closest, !doWeUseXAsKey);
+                dClosest = p.distanceSquaredTo(closest);
+            }
+            // should we check to the left/down of the splitting line?
+            if (curr.lb != null && curr.lb.rect.distanceSquaredTo(p) < dClosest) {
+                closest  = nearest(curr.lb, p, closest, !doWeUseXAsKey);
+            }
+        }
 
         return closest;
-    }
-
-    /**
-     * A nearest neighbor in the set to point p; null if the set is empty
-     * <p>
-     * Running time: log N in the best case
-     * 
-     * @param to the point to which we are looking for the closest one
-     * @return the point which is the nearest to to
-     */
-    public Point2D nearest(Point2D to) {
-        if (to == null)
-            throw new IllegalArgumentException();
-        return root != null ? nearest(root, to, root.p) : null;
     }
 
     /**
@@ -281,60 +291,66 @@ public class KdTree {
     public static void main(String[] args) {
         String filename = args[0];
         In in = new In(filename);
-        KdTree ps = new KdTree();
-        
+        KdTree kdtree = new KdTree();
+        Stopwatch watch = new Stopwatch();
         while (!in.isEmpty()) {
             double x = in.readDouble();
             double y = in.readDouble();
             Point2D p = new Point2D(x, y);
-            ps.insert(p);
+            kdtree.insert(p);
+            StdOut.printf("size: " + kdtree.size() + "\n");
+            if (kdtree.isEmpty()) throw new UnsupportedOperationException();
         }
-        StdOut.printf("size: " + ps.size() + "\n");
-
-        Point2D to = new Point2D(0.864, 0.565);
-        Point2D nearest = ps.nearest(to);
+        StdOut.printf("elapsed time: " + watch.elapsedTime() + "\n");
+        StdOut.printf("size: " + kdtree.size() + "\n");
+        // it is in input1M.txt
+        Point2D toFind = new Point2D(0.684711, 0.818767);
+        StdOut.printf("does it contain " + toFind.toString() + " ? A: " +
+         kdtree.contains(toFind) + "\n");
+        Point2D p = new Point2D(0.864, 0.565); 
+        // should be (0.864377, 0.564852) for input1M.txt
+        Point2D nearest = kdtree.nearest(p);
         StdOut.printf(nearest.toString() + " is close to " +
-                to.toString() + "\n");
-        StdOut.printf(nearest.distanceSquaredTo(to) + "\n");
-        
-        // nearest()           = (0.785, 0.725)
-        // distanceSquaredTo() = 0.031841
-        // ps.insert(new Point2D(0.372, 0.497)); // -> r v
-        // StdOut.printf("size: " + ps.size() + "\n");
-        // ps.insert(new Point2D(0.564, 0.413)); // -> r
-        // StdOut.printf("size: " + ps.size() + "\n");
-        // ps.insert(new Point2D(0.226, 0.577)); // -> r
-        // StdOut.printf("size: " + ps.size() + "\n");
-        // ps.insert(new Point2D(0.144, 0.179)); // -> r
-        // StdOut.printf("size: " + ps.size() + "\n");
-        // ps.insert(new Point2D(0.083, 0.51));
-        // StdOut.printf("size: " + ps.size() + "\n");
-        // ps.insert(new Point2D(0.32, 0.708));  // -> r
-        // StdOut.printf("size: " + ps.size() + "\n");
-        // ps.insert(new Point2D(0.417, 0.362)); // -> r
-        // StdOut.printf("size: " + ps.size() + "\n");
-        // ps.insert(new Point2D(0.862, 0.825));
-        // StdOut.printf("size: " + ps.size() + "\n");
-        // ps.insert(new Point2D(0.785, 0.725));
-        // StdOut.printf("size: " + ps.size() + "\n");
-        // ps.insert(new Point2D(0.499, 0.208)); // -> r
-        // StdOut.printf("size: " + ps.size() + "\n");
+                p.toString() + "\n");
+        // should be 1.6403299999994846E-7
+        StdOut.printf(nearest.distanceSquaredTo(p) + "\n");
+
+        // For drawing:
         // StdDraw.enableDoubleBuffering();
         // StdDraw.setXscale(0.0, 1.0);
         // StdDraw.setYscale(0.0, 1.0);
-        // ps.draw();
+        // kdtree.draw();
         // StdDraw.show();
 
-        // RectHV queryRange = new RectHV(0.1, 0.1, 0.6, 0.8);
-        // StdOut.printf("Points in range " + queryRange.toString() + ":\n");
-        // for (Point2D p : ps.range(queryRange)) {
-        //     StdOut.printf(p.toString() + "\n");
-        // }
-        
-        // Point2D to = new Point2D(0.864, 0.565);
-        // Point2D nearest = ps.nearest(to);
+        // Testing nearest
+        // correct: A B H I C F 
+        // mine:    A B H I C F (F is close)
+        // kdtree.insert(new Point2D(0.372, 0.497)); // A
+        // kdtree.insert(new Point2D(0.564, 0.413)); // B
+        // kdtree.insert(new Point2D(0.226, 0.577)); // C
+        // kdtree.insert(new Point2D(0.144, 0.179)); // D
+        // kdtree.insert(new Point2D(0.083, 0.51));  // E
+        // kdtree.insert(new Point2D(0.32, 0.708));  // F
+        // kdtree.insert(new Point2D(0.417, 0.362)); // G
+        // kdtree.insert(new Point2D(0.862, 0.825)); // H
+        // kdtree.insert(new Point2D(0.785, 0.725)); // I
+        // kdtree.insert(new Point2D(0.499, 0.208)); // J
+        // Point2D p = new Point2D(0.53, 0.76);
+        // Point2D nearest = kdtree.nearest(p);
         // StdOut.printf(nearest.toString() + " is close to " +
-        //         to.toString() + "\n");
-        // StdOut.printf(nearest.distanceSquaredTo(to) + "\n");
+        //         p.toString() + "\n");
+        // StdOut.printf(nearest.distanceSquaredTo(p) + "\n");
+
+        // A E B D & E is close
+        // kdtree.insert(new Point2D(0.7, 0.2)); // A
+        // kdtree.insert(new Point2D(0.5, 0.4)); // B
+        // kdtree.insert(new Point2D(0.2, 0.3)); // C
+        // kdtree.insert(new Point2D(0.4, 0.7)); // D
+        // kdtree.insert(new Point2D(0.9, 0.6)); // E
+        // Point2D p = new Point2D(0.8, 0.7);
+        // Point2D nearest = kdtree.nearest(p);
+        // StdOut.printf(nearest.toString() + " is close to " +
+        //         p.toString() + "\n");
+        // StdOut.printf(nearest.distanceSquaredTo(p) + "\n");
     }
 }
