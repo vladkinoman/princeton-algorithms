2fd0beb317fdbf37b80702d677b12db502f8ab4e
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2023-07-18 16:59:41 +0300

    add files for the assignment 2-2

---
+++ 2-2-seam-carving/src/SeamCarver.java
@@ -0,0 +1,61 @@
+import edu.princeton.cs.algs4.Picture;
+import java.awt.Color;
+
+public class SeamCarver {
+
+    // create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+        
+    }
+ 
+    // current picture
+    public Picture picture() {
+        return null;
+
+    }
+ 
+    // width of current picture
+    public int width() {
+        return 0;
+
+    }
+ 
+    // height of current picture
+    public int height() {
+        return 0;
+
+    }
+ 
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        return y;
+
+    }
+ 
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return null;
+
+    }
+ 
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return null;
+
+    }
+ 
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+
+    }
+ 
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+
+    }
+ 
+    //  unit testing (optional)
+    public static void main(String[] args) {
+
+    }
+ }
