21a5e574f1ea32660b40f17eadf6942760a831af
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2023-07-21 22:33:50 +0300

    add an alg for finding and removing vertical seams

--- 2-2-seam-carving/src/SeamCarver.java
+++ 2-2-seam-carving/src/SeamCarver.java
@@ -1,36 +1,75 @@
-import edu.princeton.cs.algs4.AcyclicSP;
+import java.util.Comparator;
+import java.awt.Color;
+
 import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.MinPQ;
 
 public class SeamCarver {
     private Picture picture;
-    private AcyclicSP asp;
     private double[][] aEnergy;
+    private double[][] distTo;
+    private int[][] vertexTo;
+    private MinPQ<TotalEnergyAndIndexPair> pqLastRow;
+
+    private class TotalEnergyAndIndexPair {
+        private double totalEnergy;
+        private int index;
+        public TotalEnergyAndIndexPair(double totalEnergy, int index) {
+            this.totalEnergy = totalEnergy;
+            this.index = index;
+        }
+        public double getTotalEnergy() {
+            return totalEnergy;
+        }
+        public int getIndex() {
+            return index;
+        }
+    }
+
+    private class ByTotalEnergy implements Comparator<TotalEnergyAndIndexPair> {
+
+        @Override
+        public int compare(SeamCarver.TotalEnergyAndIndexPair o1, 
+        SeamCarver.TotalEnergyAndIndexPair o2) {
+            double totalE1 = o1.getTotalEnergy();
+            double totalE2 = o2.getTotalEnergy();
+            if      (totalE1 < totalE2) return -1;
+            else if (totalE1 > totalE2) return 1;
+            return 0;
+        }
+
+    }
+
     // create a seam carver object based on the given picture
     public SeamCarver(final Picture picture) {
         validateForNull(picture);
         this.picture = new Picture(picture);
-        int w = picture.width();
-        int h = picture.height();
-        aEnergy = new double[h][];
-        for (int i = 0; i < h; i++) {
-            aEnergy[i] = new double[w];
-            for (int j = 0; j < w; j++) {
-                if (i == 0 || i == h-1 || j == 0 || j == w-1) {
+        int wid = picture.width();
+        int hei = picture.height();
+        aEnergy = new double[hei][];
+        for (int i = 0; i < hei; i++) {
+            aEnergy[i] = new double[wid];
+            for (int j = 0; j < wid; j++) {
+                if (i == 0 || i == hei-1 || j == 0 || j == wid-1) {
                     aEnergy[i][j] = 1000;
                 } else {
-                    int iRxLeft = picture.get(j, i-1).getRed();
-                    int iGxLeft = picture.get(j, i-1).getGreen();
-                    int iBxLeft = picture.get(j, i-1).getBlue();
-                    int iRxRight = picture.get(j, i+1).getRed();
-                    int iGxRight = picture.get(j, i+1).getGreen();
-                    int iBxRight = picture.get(j, i+1).getBlue();
+                    Color cLeft = picture.get(j, i-1);
+                    int iRxLeft = cLeft.getRed();
+                    int iGxLeft = cLeft.getGreen();
+                    int iBxLeft = cLeft.getBlue();
+                    Color cRight = picture.get(j, i+1);
+                    int iRxRight = cRight.getRed();
+                    int iGxRight = cRight.getGreen();
+                    int iBxRight = cRight.getBlue();
 
-                    int iRyUp = picture.get(j-1, i).getRed();
-                    int iGyUp = picture.get(j-1, i).getGreen();
-                    int iByUp = picture.get(j-1, i).getBlue();
-                    int iRyDown = picture.get(j+1, i).getRed();
-                    int iGyDown = picture.get(j+1, i).getGreen();
-                    int iByDown = picture.get(j+1, i).getBlue();
+                    Color cUp = picture.get(j-1, i);
+                    int iRyUp = cUp.getRed();
+                    int iGyUp = cUp.getGreen();
+                    int iByUp = cUp.getBlue();
+                    Color cDown = picture.get(j+1, i);
+                    int iRyDown = cDown.getRed();
+                    int iGyDown = cDown.getGreen();
+                    int iByDown = cDown.getBlue();
 
                     aEnergy[i][j] = Math.sqrt(
                         (iRxRight-iRxLeft)*(iRxRight-iRxLeft)
@@ -43,6 +82,66 @@ public class SeamCarver {
                 }
             }
         }
+
+        pqLastRow = new MinPQ<>(wid, new ByTotalEnergy());
+        
+        distTo = new double[hei][];
+        vertexTo = new int[hei][];
+        for (int i = 0; i < hei; i++) {
+            distTo[i] = new double[wid];
+            vertexTo[i] = new int[wid];
+            for (int j = 0; j < wid; j++) {
+                distTo[i][j] = Double.POSITIVE_INFINITY;
+                vertexTo[i][j] = -1;
+            }
+        }
+        for (int m = 0; m < wid; m++) {
+            distTo[0][m] = 0.0;
+            
+            for (int i = 0; i < hei; i++) {
+                for (int j = 0; j < wid; j++) {
+                    if (i == hei-1)
+                    {
+                        if (distTo[i][j] != Double.POSITIVE_INFINITY) {
+                            distTo[i][j] += aEnergy[i][j];
+                        }
+                    } else if (j == 0) {
+                        // only two edges
+                        for (int k = 0; k < 2; k++) {
+                            if (distTo[i+1][j+k] > distTo[i][j] + aEnergy[i][j])
+                            {
+                                distTo[i+1][j+k] = distTo[i][j] + aEnergy[i][j];
+                                vertexTo[i+1][j+k] = j;
+                            }
+                        }
+                    } else if (j == wid-1) {
+                        // only two edges
+                        for (int k = 0; k < 2; k++) {
+                            if (distTo[i+1][j+k-1] > distTo[i][j] + aEnergy[i][j])
+                            {
+                                distTo[i+1][j+k-1] = distTo[i][j] + aEnergy[i][j];
+                                vertexTo[i+1][j+k-1] = j;
+                            }
+                        }
+                    } else if (i != hei-1) {
+                        // three edges
+                        for (int k = 0; k < 3; k++) {
+                            if (distTo[i+1][j+k-1] > distTo[i][j] + aEnergy[i][j])
+                            {
+                                distTo[i+1][j+k-1] = distTo[i][j] + aEnergy[i][j];
+                                vertexTo[i+1][j+k-1] = j;
+                            }
+                        }
+                    }      
+                }
+            }
+
+        }
+        for (int i = 0; i < wid; i++) {
+            pqLastRow.insert(new TotalEnergyAndIndexPair(
+                                distTo[hei-1][i], i));
+        }
+        
     }
 
     private void validateForNull(Object obj) {
@@ -56,6 +155,34 @@ public class SeamCarver {
         if (x < 0 || x > picture.width() - 1) {
             throw new IllegalArgumentException(
                 "the coordinate is outside its prescribed range.");
+        }
+    }
+
+    private void validatePicture() {
+        if (picture.width() <= 1) {
+            throw new IllegalArgumentException(
+                "the width of the picture is less than or equal to 1");
+        }
+    }
+
+    private void validateSeam(final int[] seam, int hei, int wid) {
+        if (seam.length != hei) {
+            throw new IllegalArgumentException(
+                "the array of the wrong length");
+        }
+
+        for (int element : seam) {
+            if (element < 0 || element > wid-1) {
+                throw new IllegalArgumentException(
+                    "the seam entry is outside its prescribed range");
+            }
+        }
+
+        for (int i = 0; i < seam.length-1; i++) {
+            if (Math.abs(seam[i] - seam[i+1]) > 1) {
+                throw new IllegalArgumentException(
+                    "two adjacent seam entries differ by more than 1");
+            }
         }
     }
  
@@ -86,7 +213,12 @@ public class SeamCarver {
         return picture.height();
     }
 
-    // energy of pixel at column x and row y
+    /**
+     * Returns energy of pixel at column x and row y.
+     * @param x x-coordinate of pixel
+     * @param y y-coordinate of pixel
+     * @return energy of pixel at column x and row y
+     */
     public final double energy(final int x, final int y) {
         validateCoordinate(x);
         validateCoordinate(y);
@@ -96,50 +228,64 @@ public class SeamCarver {
     // sequence of indices for horizontal seam
     public final int[] findHorizontalSeam() {
         return null;
-
     }
  
     // sequence of indices for vertical seam
     public final int[] findVerticalSeam() {
-        return null;
-
+        int hei = height();
+        int[] seam = new int[hei];
+        int id = pqLastRow.min().index;
+        for (int i = hei-1; i >= 0 ; i--) {
+            seam[i] = id;
+            id = vertexTo[i][id];
+        }
+        return seam;
     }
  
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(final int[] seam) {
+        int hei = height();
+        int wid = width();
         validateForNull(seam);
-        if (seam.length != picture.width()) {
-            throw new IllegalArgumentException("the array of the wrong length");
-        }
-        // not a valid seam, throw IllegalArg here
-        // either an entry is outside its prescribed range or
-        // two adjacent entries differ by more than 1
-
-        if (picture.height() <= 1) {
-            throw new IllegalArgumentException(
-                "the height of the picture is less than or equal to 1");
-        }
+        validateSeam(seam, wid, hei);
+        validatePicture();
     }
  
     // remove vertical seam from current picture
     public void removeVerticalSeam(final int[] seam) {
+        int hei = height();
+        int wid = width();
         validateForNull(seam);
-        if (seam.length != picture.height()) {
-            throw new IllegalArgumentException("the array of the wrong length");
-        }
-        // not a valid seam, throw IllegalArg here
-        // either an entry is outside its prescribed range or
-        // two adjacent entries differ by more than 1
+        validateSeam(seam, hei, wid);
+        validatePicture();
 
-        if (picture.width() <= 1) {
-            throw new IllegalArgumentException(
-                "the width of the picture is less than or equal to 1");
+        Picture newp = new Picture(wid-1, hei);
+        for (int i = 0; i < hei; i++) {
+            int j = 0;
+            while (true) {
+                if (j == seam[i]) break;
+                newp.set(j, i, picture.get(j, i));
+                j++;
+            }
+            int k = j;
+            j++;
+            while (j < wid) {
+                newp.set(k, i, picture.get(j, i));
+                j++; k++;
+            }
         }
+
+        picture = newp;
     }
+
+
  
     //  unit testing (optional)
     public static void main(final String[] args) {
         SeamCarver sc = new SeamCarver(new Picture(args[0]));
         System.out.println(sc.energy(1, 2));
+        System.out.println(java.util.Arrays.toString(sc.findVerticalSeam()));
+        sc.removeVerticalSeam(new int[]{3, 4, 3, 2, 1});
+        sc.picture().show();
     }
  }
