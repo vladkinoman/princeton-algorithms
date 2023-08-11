fde85d692aaece4d99a3147d39796318c47ba449
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2023-08-11 22:01:55 +0300

    add files for the assignment 2-4

---
+++ 2-4-boggle/src/BoggleSolver.java
@@ -0,0 +1,37 @@
+
+public class BoggleSolver
+{
+    /**
+     * Initializes the data structure using the given array of strings as the dictionary.
+     *(You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+     * @param dictionary
+     */
+    public BoggleSolver(String[] dictionary) {
+
+    }
+    /**
+     * Returns the set of all valid words in the given Boggle board, as an Iterable.
+     * @param board
+     * @return
+     */
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        return null;
+    }
+
+    /**
+     * Returns the score of the given word if it is in the dictionary, zero otherwise.
+     * (You can assume the word contains only the uppercase letters A through Z.)
+     * @param word
+     * @return
+     */
+    public int scoreOf(String word) {
+        return 0;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+}
