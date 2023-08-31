744da39c45e63fa76f98003d829d704c71830790
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2023-08-31 18:58:30 +0300

    add files for the assignment 2-5

---
+++ 2-5-burrows–wheeler/src/BurrowsWheeler.java
@@ -0,0 +1,40 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+/**
+ * The {@code BurrowsWheeler} represents a data type for compressing.
+ *
+ * <p>
+ * This implementation uses the {@code ...} data structure which is based on...
+ * Construction takes time proportional to..
+ *
+ * @author Vlad Beklenyshchev aka vladkinoman
+ */
+public class BurrowsWheeler {
+
+    /**
+     * apply Burrows-Wheeler transform,
+     * reading from standard input and writing to standard output
+     */
+    public static void transform() {
+
+    }
+
+    /**
+     * apply Burrows-Wheeler inverse transform,
+     * reading from standard input and writing to standard output
+     */
+    public static void inverseTransform() {
+
+    }
+
+    /**
+     * Test client for BurrowsWheeler.
+     * if args[0] is "-", apply Burrows-Wheeler transform
+     * if args[0] is "+", apply Burrows-Wheeler inverse transform
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
